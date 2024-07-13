e5f173e998ae2aa9b79b276f20152b7b06d5173d
oscar-mugambi <oscarmugambi@gmail.com>
2024-07-13 20:25:58 +0300

    added auth middleware

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,14 @@
+-- name: CreateFeed :one
+INSERT INTO feed (id, name, url, user_id)
+VALUES (sqlc.arg('id'), sqlc.arg('name'), sqlc.arg('url'), sqlc.arg('user_id'))
+RETURNING *;
+
+-- -- name: GetFeed :one
+-- SELECT id, created_at, updated_at, name, url, deleted_at, user_id
+-- FROM feed
+-- WHERE id = sqlc.arg('id');
+
+-- -- name: GetFeedByUser :many
+-- SELECT id, created_at, updated_at, name, url, deleted_at, user_id
+-- FROM feed
+-- WHERE user_id = sqlc.arg('user_id');
